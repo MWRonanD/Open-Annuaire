ad3db506cb00d5759c9daa640d23cc425315d075
Alexis Leroy <alexis-60@hotmail.fr>
2017-12-08 13:51:42 +0100

    removed useless company array

--- src/app/firm-api.service.ts
+++ src/app/firm-api.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Company} from './Company';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 import {FirmApiInterface} from './firm-api-interface';
@@ -10,8 +9,8 @@ export class FirmApiService {
 
   constructor(private http: HttpClient) {
   }
+
   firmUrl = 'https://firmapi.com/api/v1/companies';
-  companies: Company[] = [];
 
   getCompanies(): Observable<FirmApiInterface> {
     return this.http.get(this.firmUrl).map(response => response as FirmApiInterface);
