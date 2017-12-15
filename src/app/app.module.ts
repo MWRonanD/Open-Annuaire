66125fea988fe124517a172e21f27dc5f3c0423e
florob95 <florob95@gmail.com>
2017-12-15 15:24:01 +0100

    import material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatSelectModule} from '@angular/material';
-import { AppComponent } from './app.component';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatSelectModule
+} from '@angular/material';
+import {AppComponent} from './app.component';
 import {TableFirmComponent} from './table-firm/table-firm.component';
-import { MenuFilterComponent } from './menu-filter/menu-filter.component';
-import { SearchBoxComponent } from './search-box/search-box.component';
+import {MenuFilterComponent} from './menu-filter/menu-filter.component';
+import {SearchBoxComponent} from './search-box/search-box.component';
 
 
 @NgModule({
@@ -33,4 +40,5 @@ import { SearchBoxComponent } from './search-box/search-box.component';
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
