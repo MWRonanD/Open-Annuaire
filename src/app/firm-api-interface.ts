49f7cc79fab1e06af83f7b987acf3ec4dfb71b27
Alexis Leroy <alexis-60@hotmail.fr>
2017-12-22 13:32:58 +0100

    changed API

--- src/app/firm-api-interface.ts
+++ src/app/firm-api-interface.ts
@@ -1,13 +1,29 @@
-import {Company} from './Company';
+import {Company} from './Model/Company';
 
 export interface FirmApiCompaniesInterface {
-  status: string;
-  params: {};
-  companies: Company[];
+  nhits: number;
+  parameters: {};
+  records:  [{
+    fields: {
+      siret: string;
+      apet700: string;
+      categorie: string;
+      depet: string;
+      libcom: string;
+      codpos: string;
+      sigle: string;
+      dcret: Date;
+      libtefen: string;
+      libreg_new: string;
+      coordonnees: [number];
+      l4_normalisee: string;
+      l1_declaree: string;
+    };
+  }];
 }
 
 export interface FirmApiCompanyInterface {
   status: string;
   param: {};
-  company: Company;
+  company: Company[];
 }
--- src/app/firm-api.service.ts
+++ src/app/firm-api.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 import {FirmApiCompanyInterface, FirmApiCompaniesInterface } from './firm-api-interface';
-import {Filter} from './Filter';
+import {Filter} from './Model/Filter';
 
 @Injectable()
 export class FirmApiService {
@@ -11,22 +11,23 @@ export class FirmApiService {
   constructor(private http: HttpClient) {
   }
   getCompany(value: string) {
-    let firmUrl = 'https://firmapi.com/api/v1/companies';
+    let firmUrl = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=sirene';
     firmUrl = firmUrl + '\/' + value;
     return this.http.get(firmUrl).map(response => response as FirmApiCompanyInterface);
   }
   getCompanies(): Observable<FirmApiCompaniesInterface> {
-    const firmUrl = 'https://firmapi.com/api/v1/companies?limit=1000';
+    const firmUrl = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=sirene';
     return this.http.get(firmUrl).map(response => response as FirmApiCompaniesInterface);
   }
 
   getCompaniesBy(filter: Filter) {
-    let firmUrl = 'https://firmapi.com/api/v1/companies?limit=1000';
+    let firmUrl = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=sirene&q=';
     const filterKeys = Object.keys(filter);
 
     for (let i = 0; i < filterKeys.length; i++) {
-      firmUrl = firmUrl + '&' + filterKeys[i] + '=' + filter[filterKeys[i]];
+      firmUrl = firmUrl + filterKeys[i] + '=' + filter[filterKeys[i]] + '&';
     }
+    console.log(firmUrl);
     return this.http.get(firmUrl).map(response => response as FirmApiCompaniesInterface);
 
   }
