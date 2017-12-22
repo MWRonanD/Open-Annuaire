b752c8da3b68da54c670e5ac056af438d68fc3ce
Alexis Leroy <alexis-60@hotmail.fr>
2017-12-22 13:33:40 +0100

    new method Company filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Company} from './Company';
+import {Company} from './Model/Company';
 import {FirmApiService} from './firm-api.service';
-import {Filter} from './Filter';
+import {Filter} from './Model/Filter';
 import {SendCompanyService} from './send-company.service';
 
 
@@ -21,18 +21,81 @@ export class AppComponent implements OnInit {
 
   searchCompanyBy(filter: Filter) {
     this.companies = [];
-    if (filter.siret !== undefined) {
-      this.firmApiService.getCompany(filter.siret).subscribe((data) => this.sendCompanyService.sendCompany(data.company));
-    } else {
-      this.firmApiService.getCompaniesBy(filter).subscribe((data) => this.sendCompanyService.sendCompanies(data.companies));
-    }
+   /* if (filter.siret !== undefined) {
+      this.firmApiService.getCompany(filter.siret).subscribe((data) => {
+        this.sendCompanyService.sendCompany(data.company);
+        this.companies.push(data.company);
+      });
+    } else { */
+      this.firmApiService.getCompaniesBy(filter).subscribe((data) => {
+        for (let i = 0; i < data.records.length; i++) {
+          const company = new Company(
+            data.records[i].fields.siret,
+            data.records[i].fields.apet700,
+            data.records[i].fields.l1_declaree,
+            data.records[i].fields.categorie,
+            data.records[i].fields.depet,
+            data.records[i].fields.l4_normalisee,
+            data.records[i].fields.libcom,
+            data.records[i].fields.codpos,
+            data.records[i].fields.sigle,
+            data.records[i].fields.dcret,
+            data.records[i].fields.libtefen,
+            data.records[i].fields.libreg_new,
+            data.records[i].fields.coordonnees);
+          this.companies.push(company);
+        }
+      });
+/*    }*/
   }
 
   ngOnInit() {
-    this.firmApiService.getCompanies().subscribe((data) => this.sendCompanyService.sendCompanies(data.companies));
+    this.companies = [];
+    this.firmApiService.getCompanies().subscribe((data) => {
+      for (let i = 0; i < data.records.length; i++) {
+        const company = new Company(
+          data.records[i].fields.siret,
+          data.records[i].fields.apet700,
+          data.records[i].fields.l1_declaree,
+          data.records[i].fields.categorie,
+          data.records[i].fields.depet,
+          data.records[i].fields.l4_normalisee,
+          data.records[i].fields.libcom,
+          data.records[i].fields.codpos,
+          data.records[i].fields.sigle,
+          data.records[i].fields.dcret,
+          data.records[i].fields.libtefen,
+          data.records[i].fields.libreg_new,
+          data.records[i].fields.coordonnees);
+        this.companies.push(company);
+      }
+      this.sendCompanyService.sendCompanies(this.companies);
+    });
   }
 
   convertFilterToCompany(filter: Filter) {
-    this.firmApiService.getCompaniesBy(filter).subscribe((data) => this.sendCompanyService.sendCompanies(data.companies));
+    this.companies = [];
+    this.firmApiService.getCompaniesBy(filter).subscribe((data) => {
+      for (let i = 0; i < data.records.length; i++) {
+        const company = new Company(
+          data.records[i].fields.siret,
+          data.records[i].fields.apet700,
+          data.records[i].fields.l1_declaree,
+          data.records[i].fields.categorie,
+          data.records[i].fields.depet,
+          data.records[i].fields.l4_normalisee,
+          data.records[i].fields.libcom,
+          data.records[i].fields.codpos,
+          data.records[i].fields.sigle,
+          data.records[i].fields.dcret,
+          data.records[i].fields.libtefen,
+          data.records[i].fields.libreg_new,
+          data.records[i].fields.coordonnees);
+        console.log(company);
+        this.companies.push(company);
+      }
+
+      this.sendCompanyService.sendCompanies(this.companies);
+    });
   }
 }
