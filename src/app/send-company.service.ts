aef552db6ad6ebef12985fd8ef4eed2c4c9b3a48
Alexis Leroy <alexis-60@hotmail.fr>
2017-12-22 13:34:14 +0100

    moved out FIlter & Company Models

--- src/app/send-company.service.ts
+++ src/app/send-company.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Subject} from 'rxjs/Subject';
-import {Company} from './Company';
+import {Company} from './Model/Company';
 import {Observable} from 'rxjs/Observable';
 
 @Injectable()
