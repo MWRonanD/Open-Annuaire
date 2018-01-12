b4a4342b7576212caf8312395f097b402edc51c2
Alexis Leroy <alexis-60@hotmail.fr>
2018-01-12 14:41:53 +0100

    changed way to retrieve parameters

--- src/app/table-firm/table-firm.component.ts
+++ src/app/table-firm/table-firm.component.ts
@@ -3,6 +3,8 @@ import {Company} from '../Model/Company';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {SendCompanyService} from '../send-company.service';
 import {Subscription} from 'rxjs/Subscription';
+import {SendUrlService} from '../send-url.service';
+import {FirmApiService} from '../firm-api.service';
 
 
 @Component({
@@ -18,11 +20,14 @@ export class TableFirmComponent {
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private sendCompanyService: SendCompanyService) {
-    this.subscription = this.sendCompanyService.getCompanies().subscribe(data => {
-      this.dataSource = new MatTableDataSource<Company>(data);
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
+  constructor(private sendCompanyService: SendCompanyService, sendUrlService: SendUrlService, firmApiService: FirmApiService) {
+    this.subscription = sendUrlService.getUrl().subscribe(data => {
+      firmApiService.searchCompanies(data).subscribe((dataCompanies) => {
+        const companies = firmApiService.convertDataToCompanies(dataCompanies);
+        this.dataSource = new MatTableDataSource<Company>(companies);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      });
     });
   }
 }
