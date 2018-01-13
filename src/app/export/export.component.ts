aa13b1394920f89f6d1cfcb3bdd8adcd576e48e8
florob95 <florob95@gmail.com>
2018-01-13 17:51:34 +0100

    exort csv with filter

--- src/app/export/export.component.ts
+++ src/app/export/export.component.ts
@@ -4,6 +4,7 @@ import {SendUrlService} from '../send-url.service';
 import {FirmApiService} from '../firm-api.service';
 import {Subscription} from 'rxjs/Subscription';
 import {DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+import {Angular2Csv} from 'angular2-csv';
 
 @Component({
   selector: 'app-export',
@@ -15,13 +16,15 @@ export class ExportComponent {
   subscription: Subscription;
   companies: Company[];
   exportjson: SafeUrl;
+  companiesNumber: number;
 
   constructor(private sendUrlService: SendUrlService, private firmApiService: FirmApiService, private sanitizer: DomSanitizer) {
     this.subscription = sendUrlService.getUrl().subscribe(data => {
       firmApiService.searchCompanies(data).subscribe((dataCompanies) => {
         this.companies = firmApiService.convertDataToCompanies(dataCompanies);
         this.exportjson = this.exportJson(this.companies);
-
+        this.companiesNumber = this.companies.length;
+        console.log(this.companies[1]);
       });
     });
   }
@@ -29,6 +32,30 @@ export class ExportComponent {
   exportJson(companies: Company[]) {
     const jsonChain = JSON.stringify(companies);
     return this.sanitizer.bypassSecurityTrustUrl('data:text/json;charset=UTF-8,' + encodeURIComponent(jsonChain));
+  }
+
+  exportCsv() {
+    const head = [
+      'adresse',
+      'ape',
+      'categorie',
+      'ville',
+      'coordonnees',
+      'date de creation',
+      'departement',
+      'forme legal',
+      'nom',
+      'nombre employees',
+      'region',
+      'siret',
+      'code postal'
+    ];
+    const options = {
+      fieldSeparator: ';',
+      headers: head
+    };
+
+     new Angular2Csv(this.companies, 'exportCsv', options);
   }
 
 
