c36d84a3cd8adc3d583fbe4a564f6abc88934850
Ronan Dhersignerie <r.dhersignerie@mentalworks.fr>
2019-03-20 16:58:28 +0100

    build

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { CONF_PROD } from './environments/environment.prod';
 import 'hammerjs';
 
-if (environment.production) {
+if (CONF_PROD.production) {
   enableProdMode();
 }
 
