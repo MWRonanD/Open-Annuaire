508fd1f8deaf52e8d0864938bcfe7c404b64ce21
Ronan Dhersignerie <r.dhersignerie@mentalworks.fr>
2019-04-03 16:16:31 +0200

    for maj

--- main.ts
+++ main.ts
@@ -58,6 +58,7 @@ function createWindow() {
 
  // win.webContents.openDevTools();
  dialog.showMessageBox({'message': 'OMG such new killer feature!!'});
+ dialog.showMessageBox({'message': 'OMG such new fucking killer feature 2!!'});
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store window
@@ -67,6 +68,33 @@ function createWindow() {
   });
 }
 
+try {
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', createWindow);
+
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow();
+    }
+  });
+} catch (e) {
+  // Catch Error
+  // throw e;
+}
 // #region Auto update
 const squirrelUrl = 'http://localhost/Installer/';
 
@@ -118,31 +146,3 @@ if (handleSquirrelEvent()) {
   // squirrel event handled and app will exit in 1000ms, so don't do anything else
 }
 // #endregion Squirrel (Auto-update)
-try {
-
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
-
-} catch (e) {
-  // Catch Error
-  // throw e;
-}
